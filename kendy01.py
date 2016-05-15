99ab2928d2f81dc7af02315cc15900b9f0e06ea8
Kevin van Rensburg <kurg@users.noreply.github.com>
2016-05-14 22:00:40 -0400

    Update kendy01.py
    adding items to the program.

--- kendy01.py
+++ kendy01.py
@@ -1,5 +1,26 @@
-# This program says hello and asks for my name.
-print('Hello world!')
-print('What is your name?')
-myName = input()
-print('It is good to meet you, ' + myName)
+# Kevin van Rensburg  @KendyVerse2016
+#This program says hello and asks for my name.
+
+import random
+import time
+
+def displayIntro():
+    print('You are in a land full of dragons. In front of you,')
+    print('you see two caves. In one cave, the dragon is friendly')
+    print('and will share his treasure with you. The other dragon')
+    print('is greedy and hungry, and will eat you on sight.')
+    print()
+
+time.sleep(5)
+    
+playAgain = 'yes'
+while playAgain == 'yes' or playAgain == 'y':
+
+    displayIntro()
+
+    # caveNumber = chooseCave()
+
+    # checkCave(caveNumber)
+
+    print('Do you want to play again? (yes or no)')
+    playAgain = input()
